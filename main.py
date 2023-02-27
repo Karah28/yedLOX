b21ec785052cf6db1fc3495c94eebb92d13d24c3
Karah28 <113633032+Karah28@users.noreply.github.com>
2023-02-27 13:51:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 a = int(input("a:"))
 b = int(input("b:"))
 print((a-b)*(a+b))
-print(a^2-2*a*b+b^2)
-print(a^2+2*a*b+b^2)
-print(a^3-3*a^2*b+3*a*b^2-b^2)
-print(a^3+3*a^2*b+3*a*b^2+b^2)
-print((a+b)*(a^2-a*b+b^2))
-print((a-b)*(a^2+a*b+b^2))
+print(a**2-2*a*b+b**2)
+print(a**2+2*a*b+b**2)
+print(a**3-3*a**2*b+3*a*b**2-b**2)
+print(a**3+3*a**2*b+3*a*b**2+b**2)
+print((a+b)*(a**2-a*b+b**2))
+print((a-b)*(a**2+a*b+b**2))
 
 
 
