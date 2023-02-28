0d16231969feb3e97c2d451ec5771dc07e5f5122
Karah28 <113633032+Karah28@users.noreply.github.com>
2023-02-28 23:45:57 +0300

    Add files via upload

--- 28.02(4).py
+++ 28.02(4).py
@@ -1,12 +1,13 @@
-x_min = int(input("Мин. X:"))
+x_min = int(input("Мини. X:"))
 x_max = int(input("Макс. X:"))
+x_min = x_min - 1
 a = int(input("a:"))
 b = int(input("b:"))
 c = int(input("c:"))
 
-while x_min <= x_max:
+while x_min != x_max:
     x_min = x_min + 1
-    y = a * (x_min) ** 2 + b * (x_min) + c
+    y = (a * (x_min) ** 2) + (b * (x_min) + c)
     print("Координаты x =", x_min, ",y =", y)
 
 
