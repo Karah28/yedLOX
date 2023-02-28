4be040b2ad870e8b91b0bfd67873568731d57b8f
Karah28 <113633032+Karah28@users.noreply.github.com>
2023-03-01 01:10:05 +0300

    Add files via upload

--- 28.02(5).py
+++ 28.02(5).py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = int(input("X:"))
-y = int(input("Y:"))
-plt.plot([x,y])
-plt.axis([0, 6, 0, 20])
-plt.ylabel('some numbers')
+x_min = int(input("Мин. X:"))
+x_max = int(input("Макс. X:"))
+x_min = x_min - 1
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
 
-plt.show()
+while x_min != x_max:
+    x_min = x_min + 1
+    y = (a * (x_min) ** 2) + (b * (x_min) + c)
+    print("Координаты x =", x_min, ",y =", y)
+
+
+    x_cords = range(x_min, x_max)
+    y_cords = [x_min * x_min for x_min in x_cords]
+
+    plt.scatter(x_cords, y_cords)
+    plt.xlim([-10, 10])
+    plt.ylim([0, 100])
+    plt.show()
