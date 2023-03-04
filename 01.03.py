f5100e500416df26695e73b3012140b1bfe9e135
Karah28 <113633032+Karah28@users.noreply.github.com>
2023-03-04 21:58:40 +0300

    Add files via upload

--- 01.03.py
+++ 01.03.py
@@ -1,30 +1,31 @@
 one = int(input("Введите первый член:"))
 diff = int(input("Введите разность прогрессии:"))
 den = int(input("Введите знаменатель прогрессии:"))
-last = int(input("Введите номер последний член:"))
+last = int(input("Введите номер последнего членa:"))
 choice = int(input("Способ решения: Формула(1) или Цикл(2)"))
 if choice == 1:
     a_n = one + ((last - 1) * diff)
 
-    b_n = one * den ** (last - 1)
+    b_n = one + ((last - 1) * diff)
 
     Summ_a = (one + a_n) * last / 2
 
-    Summ_b = (one * ((den ** b_n) - 1)) / (den - 1)
+    Summ_b = (one * ((den ** last) - 1)) / (den - 1)
     print("Арифметическая прогрессия:", Summ_a)
     print("геометрическая прогрессия:", Summ_b)
 elif choice == 2:
+    summ = 0
+    for i in range(one, last + 1):
+        s = i + diff
+        summ = s + summ
+    print("Арифметическая прогрессия:", summ)
 
-    summ = []
-    for i in range(last):
-        one = i + one
-        diff = one + diff
-    print(diff)
 
-    for i in range(last):
-         one_a = one * (den ** i)
-
-    print(one_a)
+    summ = one
+    for i in range(one, last + 1):
+        s = i * den
+        summ = s + summ
+    print("геометрическая прогрессия:", summ)
 
 
 
