6448238d8eef38c7a002992938003c96272d1277
Karah28 <113633032+Karah28@users.noreply.github.com>
2023-03-26 22:28:49 +0300

    Add files via upload

--- 22.03.py
+++ 22.03.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 graph = {'A': ['B', 'C'],
          'B': ['A', 'D', 'E'],
          'C': ['A', 'F'],
@@ -15,3 +17,28 @@ def dfs(graph, start):
     return visited
 
 print(dfs(graph, 'A'))
+
+
+
+
+visited = []
+queue = []
+def bfs(visited, graph, node):
+    global queue
+    visited.append(node)
+    queue.append(node)
+    while queue:
+        s = queue.pop(0)
+        print (s, end = " ")
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+bfs(visited, graph, 'A')
+n = 6
+visit = [False] * n
+for v in range(n):
+    if not visit[v]:
+        dfs(graph, 'A')
+print("\n", v)
